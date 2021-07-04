1f1ae46e2274aa2c52943b9572af4fdea2f7d423
jraleman <jraleman@bendiburg.com>
2021-07-04 04:40:10 -0500

    Creates itemValues data file

---
+++ data/itemValues.ts
@@ -0,0 +1,13 @@
+// pre-defined items
+export const nullValue = '000000';
+export const heartOfDaisyChain = '003300';
+export const heartOfRianov303SNR= '030E2A';
+export const heartOfRabbitWand = '030E2C';
+
+export const itemId = nullValue;
+export const amount = '00';
+
+export default ({
+    itemId,
+    amount,
+});
