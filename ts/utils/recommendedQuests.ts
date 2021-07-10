411ca81e81f6263b518edf145cbe44f031ac0cbc
jraleman <jraleman@bendiburg.com>
2021-07-10 12:06:19 -0500

    Completes recommendedQuests list

--- ts/utils/recommendedQuests.ts
+++ ts/utils/recommendedQuests.ts
@@ -1,18 +1,19 @@
+// https://wiki.pioneer2.net/index.php?title=Quests
+
 const questsByEpisode = {
-    episode1: [
+    1: [
         'forest',
         'caves',
         'mines',
         'ruins'
     ],
-    episode2: [
+    2: [
         'temple',
         'spaceship',
         'cca',
         'seabed'
     ],
-    // episode3 is a card game, reason to be skipped
-    episode4: [
+    4: [
         'crater',
         'desert'
     ],
@@ -20,60 +21,168 @@ const questsByEpisode = {
 
 const questsByArea = {
     forest: [
-        { name: 'Tyrell\'s Ego', shortName: 'TE', menuPath: 'VR >', link: 'https://wiki' },
-        { name: '???', shortName: 'RFR', menuPath: '??? >', link: 'https://guide' },
-        { name: '???', shortName: 'MAE', menuPath: '??? >', link: 'https://guide' },
+        { 
+            shortName: 'TE',
+            name: 'Tyrell\'s Ego',
+            menuPath: 'VR',
+            link: 'https://wiki.pioneer2.net/index.php?title=Tyrell%27s_Ego'
+        },
+        { 
+            shortName: 'RFR',
+            name: 'Rescue from Ragol',
+            menuPath: 'Retrieval',
+            link: 'https://wiki.pioneer2.net/index.php?title=Rescue_from_Ragol'
+        },
+        { 
+            shortName: 'MAE',
+            name: 'Maximum Attack E: Episode 1',
+            menuPath: 'Maximum Attack',
+            link: 'https://wiki.pioneer2.net/index.php?title=Maximum_Attack_E:_Episode_1'
+        },
     ],
     caves: [
-        { name: '???', shortName: 'MA1B', menuPath: '??? >', link: 'https://guide' }
+        { 
+            shortName: 'MA1B',
+            name: 'Maximum Attack 4th Stage -1B-',
+            menuPath: 'Maximum Attack',
+            link: 'https://wiki.pioneer2.net/index.php?title=Maximum_Attack_4th_Stage_-1B-'
+        },
     ],
     mines: [
-        { name: '???', shortName: 'LSB', menuPath: '??? >', link: 'https://guide' },
-        { name: '???', shortName: 'SU3', menuPath: '??? >', link: 'https://guide' },
-        { name: '???', shortName: 'EN3', menuPath: '??? >', link: 'https://guide', note: 'if good sinow ID'}
+        { 
+            shortName: 'LSB',
+            name: 'Lost SOUL BLADE',
+            menuPath: 'Retrieval',
+            link: 'https://wiki.pioneer2.net/index.php?title=Lost_SOUL_BLADE'
+        },
+        { 
+            shortName: 'SU3',
+            name: 'Sweep-up Operation #3',
+            menuPath: 'Extermination',
+            link: 'https://wiki.pioneer2.net/index.php?title=Sweep-up_Operation_3'
+        },
+        { 
+            shortName: 'EN3',
+            name: 'Endless Nightmare #3',
+            menuPath: 'Extermination',
+            link: 'https://wiki.pioneer2.net/index.php?title=Endless_Nightmare_3',
+            note: 'if good sinow ID',
+        },
     ],
     ruins: [
-        { name: 'Dark Research', shortName: 'DR', menuPath: '??? >', link: 'https://guide' },
-        { name: '???', shortName: 'EN4', menuPath: '??? >', link: 'https://guide' }
+        { 
+            shortName: 'DR',
+            name: 'Dark Research 2.0',
+            menuPath: 'Retrieval',
+            link: 'https://wiki.pioneer2.net/index.php?title=Dark_Research_2.0'
+        },
+        { 
+            shortName: 'EN4',
+            name: 'Endless Nightmare #4',
+            menuPath: 'Extermination',
+            link: 'https://wiki.pioneer2.net/index.php?title=Endless_Nightmare_4',
+        },
     ],
     temple: [
-        { name: '???', shortName: 'CCC', menuPath: '??? >', link: 'https://guide' }
+        { 
+            shortName: 'CCC',
+            name: 'CAL\'s Clock Challenge',
+            menuPath: 'Extermination',
+            link: 'https://wiki.pioneer2.net/index.php?title=CAL%27s_Clock_Challenge'
+        },
     ],
     spaceship: [
-        { name: '???', shortName: 'SU6', menuPath: '??? >', link: 'https://guide' },
-        { name: '???', shortName: 'LBA', menuPath: '??? >', link: 'https://guide' },
-        { name: '???', shortName: 'GDVD', menuPath: '??? >', link: 'https://guide' }
+        { 
+            shortName: 'SU6',
+            name: 'Sweep-up Operation #6',
+            menuPath: 'Extermination',
+            link: 'https://wiki.pioneer2.net/index.php?title=Sweep-up_Operation_6'
+        },
+        { 
+            shortName: 'LBA',
+            name: 'Lost BIND ASSAULT',
+            menuPath: 'Retrieval',
+            link: 'https://wiki.pioneer2.net/index.php?title=Lost_BIND_ASSAULT'
+        },
+        { 
+            shortName: 'GDVD',
+            name: 'Gal Da Val\'s Darkness',
+            menuPath: 'Extermination',
+            link: 'https://wiki.pioneer2.net/index.php?title=Gal_Da_Val%27s_Darkness'
+        },
     ],
     cca: [
-        { name: '???', shortName: 'MA2A', menuPath: '??? >', link: 'https://guide' },
-        { name: '???', shortName: 'MA2C', menuPath: '??? >', link: 'https://guide', note: 'if good ID' }
+        { 
+            shortName: 'MA2A',
+            name: 'Maximum Attack 4th Stage -2A-',
+            menuPath: 'Maximum Attack',
+            link: 'https://wiki.pioneer2.net/index.php?title=Maximum_Attack_4th_Stage_-2A-'
+        },
+        { 
+            shortName: 'MA2C',
+            name: 'Maximum Attack 4th Stage -2C-',
+            menuPath: 'Maximum Attack',
+            link: 'https://wiki.pioneer2.net/index.php?title=Maximum_Attack_4th_Stage_-2C-',
+            note: 'if good ID'
+        },
     ],
     seabed: [
-        { name: '???', shortName: 'SU8', menuPath: '??? >', link: 'https://guide' },
-        { name: '???', shortName: 'PW3', menuPath: '??? >', link: 'https://guide' }
+        { 
+            shortName: 'SU8',
+            name: 'Sweep-up Operation #8',
+            menuPath: 'Extermination',
+            link: 'https://wiki.pioneer2.net/index.php?title=Sweep-up_Operation_8'
+        },
+        { 
+            shortName: 'PW3',
+            name: 'Phantasmal World #3',
+            menuPath: 'Extermination',
+            link: 'https://wiki.pioneer2.net/index.php?title=Phantasmal_World_3'
+        },
     ],
     crater: [
-        { name: '???', shortName: 'MA4', menuPath: '??? >', link: 'https://guide' },
-        { name: '???', shortName: 'MAE', menuPath: '??? >', link: 'https://guide' }
+        { 
+            shortName: 'MA4A',
+            name: 'Maximum Attack 4th Stage -4A-',
+            menuPath: 'Maximum Attack',
+            link: 'https://wiki.pioneer2.net/index.php?title=Maximum_Attack_4th_Stage_-4A-'
+        },
+        { 
+            shortName: 'MAE',
+            name: 'Maximum Attack E: Episode 4',
+            menuPath: 'Maximum Attack',
+            link: 'https://wiki.pioneer2.net/index.php?title=Maximum_Attack_E:_Episode_4'
+        },
     ],
     desert: [
-        { name: '???', shortName: 'NMU3', menuPath: '??? >', link: 'https://guide' },
-        { name: '???', shortName: 'SU12', menuPath: '??? >', link: 'https://guide' }
+        { 
+            shortName: 'SU12',
+            name: 'Sweep-up Operation #12',
+            menuPath: 'Extermination',
+            link: 'https://wiki.pioneer2.net/index.php?title=Sweep-up_Operation_12'
+        },
+        { 
+            shortName: 'NMU3',
+            name: 'New Mop-Up Operation #3',
+            menuPath: 'Extermination',
+            link: 'https://wiki.pioneer2.net/index.php?title=New_Mop-Up_Operation_3'
+        },
     ]
 };
 
 // list section ids drops, and recommendation using section id by area
 const questsBySectionId = {
-
+    // EN3
+    // MA2C
 };
 
 // how many monsters, avg time, etc...
 const questsData = {
-
+    // get from quests wiki pages
 };
 
 // TODO:
 // add for questsBySectionId, questsData
-const recommendedQuests = ({ key = 'mines', sectionId = 'oran' }) => questsByArea[key];
+const recommendedQuests = ({ area, sectionId }) => questsByArea[area];
 
 export default recommendedQuests;
