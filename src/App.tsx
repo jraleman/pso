21a3c3a66b7a7941a62eac438c3697dc77d618c1
jraleman <jraleman@bendiburg.com>
2021-08-03 01:36:25 -0500

    Refactors App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+import logo from './assets/logo.svg';
 
-function App() {
+const App = () => {
+  const content = 'Edit src/App.tsx and save to reload.';
+  const footer = 'Learn React'
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header>
+        <Logo src={logo} alt="logo" />
+        <p>{content}</p>
+        <Link href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          {footer}
+        </Link>
+      </Header>
+    </Container>
   );
-}
+};
+
+const Container = styled.div`
+  text-align: center;
+`;
+
+const Logo = styled.img`
+  height: 40vmin;
+  pointer-events: none;
+  @media (prefers-reduced-motion: no-preference) {
+    animation: App-logo-spin infinite 20s linear;
+  }
+`;
+
+const Header = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const Link = styled.a`
+  color: #61dafb;
+`;
 
 export default App;
