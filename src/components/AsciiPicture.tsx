89dd321f5d5228656f2b5a6786769ad384219fe3
jraleman <jraleman@bendiburg.com>
2021-08-06 01:11:59 -0500

    Shows a picture of Al Rappy

--- src/components/AsciiPicture.tsx
+++ src/components/AsciiPicture.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 // create thumbnail with images, change to ascii (text) on hover
 // https://blog.filestack.com/tutorials/ascii-art-react/
-const AsciiPicture = ({ src, alt }: IAsciiPicture): JSX.Element => {
+const AsciiPicture = ({ src, alt = '' }: IAsciiPicture): JSX.Element => {
     return (
-        <div>AsciiPicture</div>
+        <img src={src} alt={alt} />
     );
 };
 
 export interface IAsciiPicture {
     src: string;
-    alt?: string | undefined;
+    alt?: string;
 }
 
 export default AsciiPicture;
--- src/containers/AsciiGallery.tsx
+++ src/containers/AsciiGallery.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import AsciiPicture from '../components/AsciiPicture';
+import alRappy from '../assets/rappies/al-rappy.png';
 
 // create thumbnail with images, change to ascii (text) on hover
 // take a look at meme/ repository to implement gallery
 const AsciiGallery = (): JSX.Element => {
-    const imgSrc = '';
+    const imgSrc = alRappy;
     return (
         <>
             <div>AsciiGallery</div>
