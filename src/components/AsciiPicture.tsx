bc5d10be66d0235a3c5e0f52c59d5b8977603237
jraleman <jraleman@bendiburg.com>
2021-08-07 02:44:30 -0500

    Fixes print ascii image issue

--- src/components/AsciiPicture.tsx
+++ src/components/AsciiPicture.tsx
@@ -2,44 +2,45 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import imageToAscii from '../utils/imageToAscii';
 
-const PrintAsciiLines = ({ lines = [''] }) => {
-    const print = lines.map((line) => line && <ImageLine>{line}</ImageLine>)
-    return <>{print}</>;
-};
+const uniqueId = require("lodash.uniqueid");
+
+const PrintAsciiLines = ({ lines = [''] }) => (
+    <>
+        {lines?.map((line: string) => 
+            line && (<ImageLine key={uniqueId()}>{line}</ImageLine>)
+        )}
+    </>
+);
 
 const AsciiPicture = ({ src, alt = '' }: IAsciiPicture): JSX.Element => {
     const [asciiOutput, setAsciiOutput] = useState<string | null>('');
 
     const onClick = async () => {
         const ascii = await imageToAscii({ src: src.toString() });
-        console.log('ascii', ascii);
         setAsciiOutput(ascii);
     };
 
     const lines = asciiOutput?.split('\n');
+    console.log('asciiOutput', asciiOutput);
     return (
         <ImageFrame>
             {/* change state to onHover on div */}
             <img src={src} alt={alt} />
             <button onClick={onClick}>Ascii</button>
-
-            {/* See how to print line by line and respect whitespaces in line */}
-            {/* <ImageFrame>{asciiOutput}</ImageFrame> */}
-            {/* <ImageFrame>{asciiOutput?.split('\n').map((o) => <p>{`${o}\n`}</p>)}</ImageFrame> */}
             <PrintAsciiLines lines={lines} />
         </ImageFrame>
     );
 };
 
 const ImageFrame = styled.div`
-    // font-size: 6px;
+    font-size: 6px;
     text-align: center;
-    // width: 32px;
-    // height: 32px;
 `;
 
 const ImageLine = styled.code`
     display: block;
+    max-width: 101ch;
+    white-space: pre;
 `;
 
 export interface IAsciiPicture {
--- src/utils/imageToAscii.ts
+++ src/utils/imageToAscii.ts
@@ -4,10 +4,11 @@ const imageToAscii = async ({ src }: IImageToAscii): Promise<AsciiImage> => {
     let asciiImage: AsciiImage = null;
 
     const config = {
+        // 101 chars wide
         maxWidth: 64,
         maxHeight: 64,
         // avoidedCharacters: ["#", "a", '$'],
-      };
+    };
     try {
       asciiImage = await getAsciiImage(src, config);
     } catch (err) {
